048abc8dad740fe8be12b0b0ebd321381746253b
Amogh Tiwari <amoghtiwari2000@gmail.com>
2024-01-09 16:27:02 +0530

    Added 'python3 -m ' as prefix before pip to call the right pip version in conda env

--- requirements.sh
+++ requirements.sh
@@ -1,13 +1,13 @@
-pip install opencv-python
-pip install transforms3d
-pip install pycocotools
-pip install pyrender
-pip install trimesh
-pip install pyyaml
-pip install easydict
-pip install tqdm
-pip install wandb
-pip install smplx==0.1.13
-pip install git+git://github.com/scottandrews/chumpy.git
+python3 -m pip install opencv-python
+python3 -m pip install transforms3d
+python3 -m pip install pycocotools
+python3 -m pip install pyrender
+python3 -m pip install trimesh
+python3 -m pip install pyyaml
+python3 -m pip install easydict
+python3 -m pip install tqdm
+python3 -m pip install wandb
+python3 -m pip install smplx==0.1.13
+python3 -m pip install git+git://github.com/scottandrews/chumpy.git
 git+https://github.com/mkocabas/yolov3-pytorch.git
 git+https://github.com/mkocabas/multi-person-tracker.git
